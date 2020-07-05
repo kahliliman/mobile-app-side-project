c6edb092a0f5edfaaf40b42e91259a32305993f7
kahlil iman <kahlil.iman@gmail.com>
2020-07-06 06:33:07 +0700

    finish send image to firebase functionality

---
+++ android/app/src/main/kotlin/com/kahliliman/mobile_app_side_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kahliliman.mobile_app_side_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/capture_page.dart
+++ lib/capture_page.dart
@@ -1,5 +1,9 @@
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'home_page.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'dart:async';
 import 'dart:io';
@@ -7,6 +11,9 @@ import 'dart:io';
 import 'package:camera/camera.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
+
+bool showSpinner = false;
+final _firestore = Firestore.instance;
 
 class CapturePage extends StatefulWidget {
 
@@ -48,8 +55,6 @@ class _CapturePageState extends State<CapturePage> {
         future: _initializeControllerFuture,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
-            // If the Future is complete, display the preview.
-//            return CameraPreview(_controller);
             return ClipRect(
               child: OverflowBox(
                 alignment: Alignment.center,
@@ -66,14 +71,7 @@ class _CapturePageState extends State<CapturePage> {
                 ),
               ),
             );
-
-
-
-
-
-
           } else {
-            // Otherwise, display a loading indicator.
             return Center(child: CircularProgressIndicator());
           }
           },
@@ -81,8 +79,6 @@ class _CapturePageState extends State<CapturePage> {
         floatingActionButtonLocation:FloatingActionButtonLocation.centerFloat,
         floatingActionButton: FloatingActionButton(
           backgroundColor: Color(0xff90CAF9),
-
-
           onPressed: () async {
             try {
               await _initializeControllerFuture;
@@ -108,21 +104,98 @@ class _CapturePageState extends State<CapturePage> {
   }
 }
 
-class ResultsPage extends StatelessWidget {
+//class ResultsPage extends StatelessWidget {
+//  final String imagePath;
+//
+//  const ResultsPage({Key key, this.imagePath}) : super(key: key);
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      appBar: AppBar(title: Text('Result')),
+//      // The image is stored as a file on the device. Use the `Image.file`
+//      // constructor with the given path to display the image.
+//      body: Column(
+//        children: <Widget>[
+//          Flexible(child: Image.file(File(imagePath))),
+//          FloatingActionButton.extended(
+//            backgroundColor: Color(0xff90CAF9),
+//            onPressed: () {
+//
+//
+//
+//
+//              Navigator.popUntil(context, ModalRoute.withName(Navigator.defaultRouteName));
+//            },
+//            icon: Icon(Icons.done),
+//            label: Text("Submit"),
+//          ),
+//        ],
+//      )
+//    );
+//  }
+//}
+
+class ResultsPage extends StatefulWidget {
   final String imagePath;
 
-  const ResultsPage({Key key, this.imagePath}) : super(key: key);
+  ResultsPage({Key key, this.imagePath}) : super(key: key);
+  @override
+  _ResultsPageState createState() => _ResultsPageState();
+}
+
+class _ResultsPageState extends State<ResultsPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Result')),
-      // The image is stored as a file on the device. Use the `Image.file`
-      // constructor with the given path to display the image.
-      body: Column(
-        children: <Widget>[
-          Image.file(File(imagePath)),
-        ],)
+    return ModalProgressHUD(
+      inAsyncCall: showSpinner,
+      child: Scaffold(
+          appBar: AppBar(title: Text('Result')),
+          // The image is stored as a file on the device. Use the `Image.file`
+          // constructor with the given path to display the image.
+          body: Column(
+            children: <Widget>[
+              Flexible(child: Image.file(File(widget.imagePath))),
+              FloatingActionButton.extended(
+                backgroundColor: Color(0xff90CAF9),
+                onPressed: () {
+
+                  try {
+                    setState(() {
+                      showSpinner = true;
+                    });
+
+                    final StorageReference firebaseStorageRef = FirebaseStorage.instance.ref().child('${DateTime.now()}.png');
+                    final StorageUploadTask task = firebaseStorageRef.putFile(File(widget.imagePath));
+
+                    _firestore.collection('capture').add({
+                      'created_at': DateTime.now(),
+                      'path': 'gs://remote-attendance-mobile.appspot.com/${DateTime.now()}.png'
+                    });
+
+                    Navigator.popUntil(context, ModalRoute.withName(Navigator.defaultRouteName));
+                  }
+
+                  catch (e) {
+                    print(e);
+
+                    setState(() {
+                      showSpinner = false;
+                    });
+                  }
+                },
+                icon: Icon(Icons.done),
+                label: Text("SUBMIT"),
+              ),
+//              FloatingActionButton.extended(
+//                onPressed: null,
+//                label: Text("RETAKE"),
+//                icon: Icon(Icons.refresh),
+//              )
+            ],
+          )
+      ),
     );
   }
 }
